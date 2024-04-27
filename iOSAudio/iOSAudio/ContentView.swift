a98e9ae5fe8d683c60495d9352c19a724ffa7266
Novikov Nikita <sirpointer@gmail.com>
2024-04-27 10:25:01 +0400

    create project

---
+++ iOSAudio/iOSAudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSAudio
+//
+//  Created by Novikov Nikita on 27.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSAudio/iOSAudio/iOSAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAudioApp.swift
+//  iOSAudio
+//
+//  Created by Novikov Nikita on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
